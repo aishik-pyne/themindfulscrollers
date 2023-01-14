d1645490be6ca29efe672068747a5e1a043c1222
aishik-pyne <aishikpyne@gmail.com>
2023-01-14 18:23:44 +0800

    Fixing ribbon to a modal

--- extension/background.js
+++ extension/background.js
@@ -1,11 +1,11 @@
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status == "complete") {
-        chrome.tabs.executeScript({
-            file: 'contentScript.js',
-        })
         chrome.tabs.insertCSS({
             file: 'contentScript.css'
+        })
+        chrome.tabs.executeScript({
+            file: 'contentScript.js',
         })
     }
     
