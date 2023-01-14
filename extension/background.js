48aec55ab0fe30bf64b71cc74eeca7e5c148a9f6
Niharika Shrivastava <chunnushrivastava@gmail.com>
2023-01-14 21:27:13 +0800

    Convert into json

--- extension/background.js
+++ extension/background.js
@@ -10,3 +10,32 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     }
     
 })
+
+const storageCache = {count:0};
+fetch('correlation.json')
+    .then((response) => response.json())
+    .then((json) => {
+        Object.assign(storageCache, json);
+        chrome.storage.local.set({'correlation_matrix': json}, function() {
+            chrome.storage.local.get("correlation_matrix", function(items) {
+                console.log(Object.keys(items["correlation_matrix"]));
+            });
+
+            // var entropy = function () {
+            //     return storageCache['Music']['Classics'];
+            //   };
+            // console.log(entropy());
+            
+        });
+    });
+
+
+
+// chrome.storage.local.set({'foo': 'hello', 'bar': 'hi'}, function() {
+//     console.log('Settings saved');
+//   });
+
+// // Read it using the storage API
+// chrome.storage.local.get(['foo', 'bar'], function(items) {
+//     console.log('Settings retrieved', items);
+// });
